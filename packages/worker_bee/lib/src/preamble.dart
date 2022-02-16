608d019bc9dd5c9e2986d5cf35c638f9316e9d7b
Dillon Nys <nydillon@amazon.com>
2022-02-16 11:28:42 -0700

    chore(runtime): Update hive definition

--- packages/worker_bee/lib/src/preamble.dart
+++ packages/worker_bee/lib/src/preamble.dart
@@ -1,21 +1,25 @@
 import 'dart:async';
 
+import 'package:worker_bee/worker_bee.dart';
+
 import 'vm/preamble.dart' if (dart.library.html) 'js/preamble.dart';
+
+typedef WorkerBeeBuilder<W extends WorkerBeeBase> = W Function();
 
 /// Initializes worker bees by checking if running in a web worker, and awaiting
 /// the assigned role if so.
 Future<void> runHive<R>(
-  Map<String, Future<void> Function()> workers,
+  Map<String, WorkerBeeBuilder> workers,
   FutureOr<void> Function() runApp,
 ) async {
   if (isWebWorker) {
     print('(Worker) Getting assignment...');
     final assignment = await getWorkerAssignment();
-    final runner = workers[assignment];
-    if (runner == null) {
+    final worker = workers[assignment];
+    if (worker == null) {
       throw StateError('No worker found for role: $assignment');
     }
-    await runner();
+    await worker().connect();
   } else {
     print('(Main) Not a web worker. Running app...');
     await runApp();
