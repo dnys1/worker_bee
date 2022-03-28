2f89188b9116872ad2915e104ed00cf8cde022ab
Dillon Nys <nydillon@amazon.com>
2022-03-28 09:18:00 -0700

    chore: Update example

--- example/lib/example.worker.vm.dart
+++ example/lib/example.worker.vm.dart
@@ -4,14 +4,14 @@ import 'dart:isolate';
 import 'package:example/example.dart';
 import 'package:worker_bee/worker_bee.dart';
 
-Future<void> _run(SendPort sendPort) async {
-  final channel = IsolateChannel<MyMessage>.connectSend(sendPort);
+Future<void> _run(SendPorts ports) async {
+  final channel = IsolateChannel<Object>.connectSend(ports.messagePort);
   final result = await MyWorkerImpl().run(
-    channel.stream,
-    channel.sink,
+    channel.stream.cast(),
+    channel.sink.cast(),
   );
   safePrint('(Worker) Finished with result: $result');
-  Isolate.exit(sendPort, result);
+  Isolate.exit(ports.exitPort, result);
 }
 
 class MyWorkerImpl extends MyWorker {
