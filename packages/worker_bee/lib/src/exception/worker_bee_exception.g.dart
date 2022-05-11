01aa931e0adbbb01cf083805375b124c7b3dce1d
Dillon Nys <nydillon@amazon.com>
2022-05-10 18:57:47 -0700

    chore: Update built_value_generator

--- packages/worker_bee/lib/src/exception/worker_bee_exception.g.dart
+++ packages/worker_bee/lib/src/exception/worker_bee_exception.g.dart
@@ -47,13 +47,13 @@ class _$WorkerBeeExceptionImplSerializer
 
     final iterator = serialized.iterator;
     while (iterator.moveNext()) {
-      final key = iterator.current as String;
+      final key = iterator.current! as String;
       iterator.moveNext();
       final Object? value = iterator.current;
       switch (key) {
         case 'error':
           result.error = serializers.deserialize(value,
-              specifiedType: const FullType(String)) as String;
+              specifiedType: const FullType(String))! as String;
           break;
         case 'stackTrace':
           result.stackTrace = serializers.deserialize(value,
@@ -84,7 +84,7 @@ class _$WorkerBeeExceptionImpl extends WorkerBeeExceptionImpl {
 
   factory _$WorkerBeeExceptionImpl(
           [void Function(WorkerBeeExceptionImplBuilder)? updates]) =>
-      (new WorkerBeeExceptionImplBuilder()..update(updates)).build();
+      (new WorkerBeeExceptionImplBuilder()..update(updates))._build();
 
   _$WorkerBeeExceptionImpl._({required this.error, this.stackTrace})
       : super._() {
@@ -162,7 +162,9 @@ class WorkerBeeExceptionImplBuilder
   }
 
   @override
-  _$WorkerBeeExceptionImpl build() {
+  WorkerBeeExceptionImpl build() => _build();
+
+  _$WorkerBeeExceptionImpl _build() {
     final _$result = _$v ??
         new _$WorkerBeeExceptionImpl._(
             error: BuiltValueNullFieldError.checkNotNull(
@@ -173,4 +175,4 @@ class WorkerBeeExceptionImplBuilder
   }
 }
 
-// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
+// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,no_leading_underscores_for_local_identifiers,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
--- packages/worker_bee/test/custom_type.g.dart
+++ packages/worker_bee/test/custom_type.g.dart
@@ -33,13 +33,13 @@ class _$CustomTypeSerializer implements StructuredSerializer<CustomType> {
 
     final iterator = serialized.iterator;
     while (iterator.moveNext()) {
-      final key = iterator.current as String;
+      final key = iterator.current! as String;
       iterator.moveNext();
       final Object? value = iterator.current;
       switch (key) {
         case 'customField':
           result.customField = serializers.deserialize(value,
-              specifiedType: const FullType(String)) as String;
+              specifiedType: const FullType(String))! as String;
           break;
       }
     }
@@ -53,7 +53,7 @@ class _$CustomType extends CustomType {
   final String customField;
 
   factory _$CustomType([void Function(CustomTypeBuilder)? updates]) =>
-      (new CustomTypeBuilder()..update(updates)).build();
+      (new CustomTypeBuilder()..update(updates))._build();
 
   _$CustomType._({required this.customField}) : super._() {
     BuiltValueNullFieldError.checkNotNull(
@@ -116,7 +116,9 @@ class CustomTypeBuilder implements Builder<CustomType, CustomTypeBuilder> {
   }
 
   @override
-  _$CustomType build() {
+  CustomType build() => _build();
+
+  _$CustomType _build() {
     final _$result = _$v ??
         new _$CustomType._(
             customField: BuiltValueNullFieldError.checkNotNull(
@@ -126,4 +128,4 @@ class CustomTypeBuilder implements Builder<CustomType, CustomTypeBuilder> {
   }
 }
 
-// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
+// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,deprecated_member_use_from_same_package,lines_longer_than_80_chars,no_leading_underscores_for_local_identifiers,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
